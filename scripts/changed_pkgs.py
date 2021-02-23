7444aa4f4f3c809a4de543bcdd336ebf51b45da9
Patrik Obrist <padx@gmx.net>
2021-02-23 18:47:38 +0100

    add changed_pkgs.py script

--- scripts/changed_pkgs.py
+++ scripts/changed_pkgs.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 
 import os
+import re
 
 lines = os.popen('git diff HEAD~ --name-only').readlines()
+p = re.compile('^recipes/([^/]*)/.*$')
+
+pkgs = set()
 
 for line in lines:
   line = line.strip()
-  print('line:', line)
+  pkgs.update(p.findall(line))
 
+pkgs = sorted(pkgs)
 
+print('\n'.join(pkgs))
